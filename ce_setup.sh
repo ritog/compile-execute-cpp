48c8baa3b4235552195a019cea7713beeb38440e
Ritobrata Ghosh <coder.rg7@gmail.com>
2020-10-05 13:56:46 +0530

    adding chmod to bashrc

--- ce_setup.sh
+++ ce_setup.sh
@@ -15,11 +15,11 @@ echo 'alias ce="ce.sh"' >> ~/.bashrc
 
 echo 'Alias ce set...'
 
-# Applying changes to current terminal
-source ~/.bashrc
-
-echo 'Changes applied to current terminal session...'
+# Giving the main script permission to execute
+echo 'chmod +x $HOME/.ce-cpp/ce.sh' >> ~/.bashrc
 
 echo 'SETUP COMPLETE'
 
-echo 'You can now use ce-cpp by running `ce <file-name>.cpp` to compile and execute a file with one command'
+echo 'Restart current terminal session or start a new one to start using ce-cpp'
+
+echo 'You can now use ce-cpp by running `ce <file-name>.cpp` to compile and execute a file with one command.'
